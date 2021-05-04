a97bde53a1f08295c265677b08452ad7d7efeddd
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-04 23:52:49 +0530

    basic animation

--- framer-motion/src/components/Home.js
+++ framer-motion/src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
     <div className="home container">
-      <h2>
+      <motion.h2 animate={{ fontSize: 50 }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
     </div>
   )
