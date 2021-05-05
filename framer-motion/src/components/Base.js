129e430fa4d5a4f7ade88aa2465b331ca7f451b7
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-05 13:10:45 +0530

    variants-1

--- framer-motion/src/components/Base.js
+++ framer-motion/src/components/Base.js
@@ -2,12 +2,40 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw',
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+  }
+}
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw',
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: 'spring', stiffness: 120
+    }
+  }
+}
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
-    <motion.div className="base container" initial={{ x: '100vw' }} animate={{ x: 0 }} transition={{ type: 'tween', delay: 0.5, duration: 1 }}>
+    // initial={{ x: '100vw' }} animate={{ x: 0 }}
 
+    <motion.div className="base container" variants={containerVariants} initial='hidden' animate='visible' transition={{ type: 'tween', delay: 0.5, duration: 1 }}>
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map(base => {
