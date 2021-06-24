e56381b410f15f156be155357350c857c8f0ee8f
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-24 23:56:46 +0530

    global styles added

---
+++ movie-db/src/App.js
@@ -0,0 +1,12 @@
+import {GlobalStyles} from './GlobalStyles'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Movie App</h1>
+     <GlobalStyles/>
+    </div>
+  );
+}
+
+export default App;
---
+++ movie-db/src/GlobalStyles.js
@@ -0,0 +1,40 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyles=createGlobalStyle`
+:root{
+    --maxWidth:1280px;
+    --light:#fff;
+    --smGray:#eee;
+    --mdGray:#353535;
+    --lgGray:#1c1c1c;
+    --fontXl:2.5rem;
+    --fontLg:1.5rem;
+    --fontMd:1.2rem;
+    --fontSm:1rem;
+}
+
+*{
+    box-sizing: border-box !important ;
+    margin: 0;
+    padding: 0;
+}
+body{
+    margin: 0;
+    padding: 0;
+    
+    h1{
+        font-size: 2rem;
+        font-weight: 600;
+        color: var(---light);
+    }
+    h3{
+        font-size: 1.1rem;
+        font-weight: 600;
+    }
+    p{
+        font-size: 1rem;
+        color: var(---light);
+    }
+}
+`
+
